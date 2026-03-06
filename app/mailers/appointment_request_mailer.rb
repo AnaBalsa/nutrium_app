5daf0f7dc56d3c3c699a14c939203764b2f59bae
AnaBalsa <ana.carvalho.balsa@gmail.com>
2026-03-06 00:22:02 +0000

    Update mailer "from" email

--- app/mailers/appointment_request_mailer.rb
+++ app/mailers/appointment_request_mailer.rb
@@ -2,8 +2,12 @@ class AppointmentRequestMailer < ApplicationMailer
   def send_notification(appointment_request)
     @appointment_request = appointment_request
 
+    nutritionist = appointment_request.service.nutritionist
+    from_email = "#{nutritionist.name.parameterize}@gmail.com"
+
     mail(
       to: @appointment_request.guest_email,
+      from: from_email,
       subject: "Your appointment request was #{@appointment_request.status}"
     )
   end
