279c27737e32fba9d7b96eeb37d37ca302f7a493
AnaBalsa <ana.carvalho.balsa@gmail.com>
2026-03-06 00:16:40 +0000

    Update enum syntax to new rails syntax

--- app/models/appointment_request.rb
+++ app/models/appointment_request.rb
@@ -2,7 +2,7 @@ class AppointmentRequest < ApplicationRecord
   belongs_to :nutritionist
   belongs_to :service
 
-  enum status: { pending: 0, accepted: 1, rejected: 2 }
+  enum :status, { pending: 0, accepted: 1, rejected: 2 }
 
   validates :guest_name, :guest_email, :starts_at, :ends_at, :status, presence: true
   validate :ends_after_starts
